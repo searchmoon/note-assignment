869fd920841610e5449b6ac076fea30094f9b7ed
Rumi <moonding901212@gmail.com>
2023-12-12 15:03:37 +0900

    lexical 텍스트 편집기 수정

--- src/components/textEditor/TextEditor.jsx
+++ src/components/textEditor/TextEditor.jsx
@@ -1,59 +1,60 @@
-import { useEffect } from "react";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-import "../textEditor/textEditor.css";
-import useDebounce from "../hooks/useDebounce";
-import onChange from "./plugins/onChange";
+// import editorConfig from "./editorConfig";
+import { EmojiNode } from "./nodes/EmojiNode";
+import ExampleTheme from "./themes/ExampleTheme";
+import { $getRoot, $getSelection } from "lexical";
 
-const theme = {
-  ltr: "ltr",
-  rtl: "rtl",
-  placeholder: "editor-placeholder",
-  paragraph: "editor-paragraph",
+import TreeViewPlugin from "./plugins/TreeViewPlugin";
+import EmoticonPlugin from "./plugins/EmoticonPlugin";
+import MyCustomAutoFocusPlugin from "./plugins/MyCustomAutoFocusPlugin";
+// import onChange from "./plugins/onChange";
+
+const editorConfig = {
+  theme: ExampleTheme,
+  onError(error) {
+    throw error;
+  },
+  nodes: [EmojiNode],
 };
 
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
+// When the editor changes, you can get notified via the
+// LexicalOnChangePlugin!
+const onChange = (editorState) => {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
 
-  useEffect(() => {
-    editor.focus();
-  }, [editor]);
+    console.log(root, selection);
+  });
+};
 
-  return null;
+function Placeholder() {
+  return <div className="editor-placeholder">Enter some plain text...</div>;
 }
 
-function onError(error) {
-  console.error(error);
-}
-
-function TextEditor() {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme,
-    onError,
-  };
-
+const TextEditor = () => {
   return (
-    <div className="editor-container">
-      <LexicalComposer initialConfig={initialConfig}>
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="editor-container">
         <PlainTextPlugin
-          contentEditable={<ContentEditable />}
-          placeholder={
-            <div className="editor-placeholder">Type / for menu or select from Templates</div>
-          }
+          contentEditable={<ContentEditable className="editor-input" />}
+          placeholder={<Placeholder />}
           ErrorBoundary={LexicalErrorBoundary}
         />
+        <OnChangePlugin onChange={onChange} />
         <HistoryPlugin />
-        <OnChangePlugin onChange={useDebounce(onChange, 1000)} />
+        <TreeViewPlugin />
+        <EmoticonPlugin />
         <MyCustomAutoFocusPlugin />
-      </LexicalComposer>
-    </div>
+      </div>
+    </LexicalComposer>
   );
-}
+};
 
 export default TextEditor;
--- src/components/textEditor/plugins/onChange.js
+++ src/components/textEditor/plugins/onChange.js
@@ -1,21 +1,35 @@
+// import { $getRoot, $getSelection } from "lexical";
+
+// export default function onChange(editorState) {
+//   editorState.read(() => {
+//     const root = $getRoot();
+//     const selection = $getSelection();
+
+//     console.log(root, selection);
+//     console.log(root.__cachedText);
+
+//     const date = new Date();
+
+//     localStorage.setItem(
+//       "textInfo",
+//       JSON.stringify({
+//         text: root.__cachedText,
+//         timestamp: date.toISOString().slice(0, 19),
+//       })
+//     );
+//   });
+// }
+
 import { $getRoot, $getSelection } from "lexical";
 
+// When the editor changes, you can get notified via the
+// LexicalOnChangePlugin!
 export default function onChange(editorState) {
   editorState.read(() => {
+    // Read the contents of the EditorState here.
     const root = $getRoot();
     const selection = $getSelection();
 
     console.log(root, selection);
-    console.log(root.__cachedText);
-
-    const date = new Date();
-
-    localStorage.setItem(
-      "saveTextInfo",
-      JSON.stringify({
-        text: root.__cachedText,
-        timestamp: date.toISOString(),
-      })
-    );
   });
 }
